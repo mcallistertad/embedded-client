33d68b873ff14b22d1a9d4286d87cf420a557e18
Ted Boinske <tboinske@gmail.com>
2019-03-06 22:05:04 -0500

    Add more server stuff

--- pyutils/server.py
+++ pyutils/server.py
@@ -1,40 +1,70 @@
 #!/usr/bin/env python
 import socketserver
+import threading
 import logging
 import elg_proto
 
-logging.basicConfig(format='%(asctime)s: %(message)s', filename='server.log',level=logging.DEBUG)
+PORT=9756
+SOCKET_TIMEOUT=5
 
-class MyTCPHandler(socketserver.BaseRequestHandler):
+#logging.basicConfig(format='%(asctime)s: %(message)s', filename='server.log',level=logging.DEBUG)
+logging.basicConfig(format='%(asctime)s [%(threadName)s] %(message)s', level=logging.DEBUG)
+
+class RequestHandler(socketserver.BaseRequestHandler):
     def handle(self):
-        buf = bytearray()
+        logging.info("Handling request. Active thread count = {}".format(threading.active_count()))
 
-        while len(buf) < elg_proto.RQ_HEADER_LEN:
-            buf.extend(self.request.recv(elg_proto.RQ_HEADER_LEN))
+        self.request.settimeout(SOCKET_TIMEOUT)
 
-        header = elg_proto.decode_rq_header(buf)
+        try:
+            # Read the header in order to get the partner_id and to determine
+            # how long the message is.
+            buf = bytearray()
 
-        logging.info("---- header: ----\n" + str(header))
+            while len(buf) < elg_proto.RQ_HEADER_LEN:
+                buf.extend(self.request.recv(elg_proto.RQ_HEADER_LEN - len(buf)))
 
-        buf = bytearray()
+            header = elg_proto.decode_rq_header(buf)
 
-        while len(buf) < header.remaining_length:
-            buf.extend(self.request.recv(header.remaining_length))
+            logging.info("---- header: ----\n" + str(header))
 
-        logging.info("remaining bytes read: {}".format(len(buf)))
+            # Read the rest of the message.
+            buf = bytearray()
 
-        # TODO: determine the key based on the value of header.partner_id.
-        key = b'\x00\x01\x02\x03\x04\x05\x06\x07\x08\x09\x0a\x0b\x0c\x0d\x0e\x0f'
+            while len(buf) < header.remaining_length:
+                buf.extend(self.request.recv(header.remaining_length - len(buf)))
 
-        body = elg_proto.decode_rq_crypto_info_and_body(buf, key)
+            logging.info("remaining bytes read: {}".format(len(buf)))
 
-        logging.info("---- body: ----\n" + str(body))
+            # TODO: determine the key based on the value of header.partner_id.
+            key = b'\x00\x01\x02\x03\x04\x05\x06\x07\x08\x09\x0a\x0b\x0c\x0d\x0e\x0f'
+
+            # Read and decode the remainder of the message.
+            body = elg_proto.decode_rq_crypto_info_and_body(buf, key)
+
+            logging.info("---- body: ----\n" + str(body))
+
+            # TODO: Create the corresponding API server request, send it
+            # thither, get the API server response, create and send the client
+            # response.
+
+            logging.info("Request complete")
+        except Exception as e:
+            logging.error("exception: " + str(e))
+
+
+class ThreadedTCPServer(socketserver.ThreadingMixIn, socketserver.TCPServer):
+    pass
 
 
 if __name__ == "__main__":
-    HOST, PORT = "localhost", 9755
-
-    server = socketserver.TCPServer((HOST, PORT), MyTCPHandler)
+    server = ThreadedTCPServer(("localhost", PORT), RequestHandler)
     server.allow_reuse_address = True
 
-    server.serve_forever()
+    with server:
+        listener_thread = threading.Thread(target=server.serve_forever)
+
+        listener_thread.daemon = True
+        listener_thread.start()
+
+        server.serve_forever()
