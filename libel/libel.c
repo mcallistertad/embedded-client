mcallistertad/embedded-client

commit 655b1e6dffbc2df5616a3a730e98921e021774e8

[ELG-197] sky_open includes callback fn for time
Added example callback fn in unit_test.c and sample_client.c

libel/libel.c
@@ -34,6 +34,7 @@ static Sky_cache_t cache;
 static Sky_randfn_t sky_rand_bytes;
 static Sky_loggerfn_t sky_logf;
 static Sky_log_level_t sky_min_level;
+static Sky_timefn_t sky_time;
 
 /* Local functions */
 static bool validate_device_id(uint8_t *device_id, uint32_t id_len);
@@ -53,6 +54,7 @@ static bool validate_aes_key(uint8_t aes_key[AES_SIZE]);
  *  @param min_level logging function is called for msg with equal or greater level
  *  @param logf pointer to logging function
  *  @param rand_bytes pointer to random function
+ *  @param gettime pointer to time function
  *
  *  @return sky_status_t SKY_SUCCESS or SKY_ERROR
  *
@@ -64,7 +66,7 @@ static bool validate_aes_key(uint8_t aes_key[AES_SIZE]);
 Sky_status_t sky_open(Sky_errno_t *sky_errno, uint8_t *device_id,
     uint32_t id_len, uint32_t partner_id, uint32_t aes_key_id,
     uint8_t aes_key[16], void *state_buf, Sky_log_level_t min_level,
-    Sky_loggerfn_t logf, Sky_randfn_t rand_bytes)
+    Sky_loggerfn_t logf, Sky_randfn_t rand_bytes, Sky_timefn_t gettime)
 {
     Sky_cache_t *sky_state = state_buf;
     int i = 0;
@@ -75,6 +77,11 @@ Sky_status_t sky_open(Sky_errno_t *sky_errno, uint8_t *device_id,
 
     if (sky_state != NULL && !validate_cache(sky_state))
         sky_state = NULL;
+
+    sky_min_level = min_level;
+    sky_logf = logf;
+    sky_rand_bytes = rand_bytes == NULL ? sky_rand_fn : rand_bytes;
+    sky_time = (gettime == NULL) ? &time : gettime;
 
     /* if open already */
     if (sky_open_flag && sky_state) {
@@ -94,7 +101,7 @@ Sky_status_t sky_open(Sky_errno_t *sky_errno, uint8_t *device_id,
     else {
         cache.header.magic = SKY_MAGIC;
         cache.header.size = sizeof(cache);
-        cache.header.time = (uint32_t)time(NULL);
+        cache.header.time = (uint32_t)(*sky_time)(NULL);
         cache.header.crc32 = sky_crc32(&cache.header.magic,
             (uint8_t *)&cache.header.crc32 - (uint8_t *)&cache.header.magic);
         cache.len = CACHE_SIZE;
@@ -115,9 +122,6 @@ Sky_status_t sky_open(Sky_errno_t *sky_errno, uint8_t *device_id,
         return sky_return(sky_errno, SKY_ERROR_BAD_PARAMETERS);
 
     cache.sky_id_len = id_len;
-    sky_min_level = min_level;
-    sky_logf = logf;
-    sky_rand_bytes = rand_bytes == NULL ? sky_rand_fn : rand_bytes;
     memcpy(cache.sky_device_id, device_id, id_len);
     cache.sky_partner_id = partner_id;
     cache.sky_aes_key_id = aes_key_id;
@@ -191,14 +195,14 @@ Sky_ctx_t *sky_new_request(
     /* update header in workspace */
     ctx->header.magic = SKY_MAGIC;
     ctx->header.size = bufsize;
-    ctx->header.time = (uint32_t)time(NULL);
+    ctx->header.time = (uint32_t)(*sky_time)(NULL);
     ctx->header.crc32 = sky_crc32(&ctx->header.magic,
         (uint8_t *)&ctx->header.crc32 - (uint8_t *)&ctx->header.magic);
 
     ctx->cache = &cache;
     ctx->logf = sky_logf;
     ctx->rand_bytes = sky_rand_bytes;
-    ctx->min_level = sky_min_level;
+    ctx->gettime = sky_time;
     ctx->len = 0; /* empty */
     ctx->ap_len = 0; /* empty */
     for (i = 0; i < TOTAL_BEACONS; i++)
@@ -588,7 +592,7 @@ Sky_status_t sky_decode_response(Sky_ctx_t *ctx, Sky_errno_t *sky_errno,
     if (deserialize_response(ctx, response_buf, bufsize, loc) < 0)
         return sky_return(sky_errno, SKY_ERROR_LOCATION_UNKNOWN);
 
-    loc->time = time(NULL);
+    loc->time = (*ctx->gettime)(NULL);
 
     logfmt(ctx, SKY_LOG_LEVEL_DEBUG, "%s", __FUNCTION__);
 

libel/beacons.c
@@ -361,9 +361,10 @@ int find_best_match(Sky_ctx_t *ctx, bool put)
     /* score each cache line wrt beacon match ratio */
     for (i = 0; i < CACHE_SIZE; i++) {
         ratio[i] = score[i] = 0;
-        if (put && (ctx->cache->cacheline[i].time == 0 ||
-                       (uint32_t)time(NULL) - ctx->cache->cacheline[i].time >
-                           (CACHE_AGE_THRESHOLD * 60 * 60))) {
+        if (put &&
+            (ctx->cache->cacheline[i].time == 0 ||
+                (uint32_t)(*ctx->gettime)(NULL)-ctx->cache->cacheline[i].time >
+                    (CACHE_AGE_THRESHOLD * 60 * 60))) {
             /* looking for match for put, empty cacheline = 1st choice */
             score[i] = TOTAL_BEACONS * 2;
         } else if (!put && ctx->cache->cacheline[i].time == 0) {
@@ -452,11 +453,11 @@ int find_oldest(Sky_ctx_t *ctx)
 {
     int i;
     uint32_t oldestc = 0;
-    int oldest = time(NULL);
+    int oldest = (*ctx->gettime)(NULL);
 
     for (i = 0; i < CACHE_SIZE; i++) {
         /* Discard old cachelines */
-        if ((uint32_t)time(NULL) - ctx->cache->cacheline[i].time >
+        if ((uint32_t)(*ctx->gettime)(NULL)-ctx->cache->cacheline[i].time >
             (CACHE_AGE_THRESHOLD * 60 * 60)) {
             ctx->cache->cacheline[i].time = 0;
             return i;
@@ -484,7 +485,7 @@ Sky_status_t add_cache(Sky_ctx_t *ctx, Sky_location_t *loc)
 {
     int i = -1;
     int j;
-    uint32_t now = time(NULL);
+    uint32_t now = (*ctx->gettime)(NULL);
 
     /* compare current time to Mar 1st 2019 */
     if (now <= 1551398400) {
@@ -536,7 +537,7 @@ Sky_status_t add_cache(Sky_ctx_t *ctx, Sky_location_t *loc)
  */
 int get_cache(Sky_ctx_t *ctx)
 {
-    uint32_t now = time(NULL);
+    uint32_t now = (*ctx->gettime)(NULL);
 
     /* compare current time to Mar 1st 2019 */
     if (now <= 1551398400) {

libel/libel.h
@@ -95,6 +95,18 @@ typedef enum {
     SKY_LOG_LEVEL_ALL = SKY_LOG_LEVEL_DEBUG,
 } Sky_log_level_t;
 
+/*! \brief pointer to logger callback function
+ */
+typedef int (*Sky_loggerfn_t)(Sky_log_level_t level, const char *s);
+
+/*! \brief pointer to random bytes callback function
+ */
+typedef int (*Sky_randfn_t)(uint8_t *rand_buf, uint32_t bufsize);
+
+/*! \brief pointer to gettime callback function
+ */
+typedef time_t (*Sky_timefn_t)(time_t *t);
+
 #ifndef SKY_LIBEL
 typedef void Sky_ctx_t;
 #else
@@ -105,18 +117,10 @@ typedef void Sky_ctx_t;
 #include "utilities.h"
 #endif
 
-/*! \brief pointer to logger callback function
- */
-typedef int (*Sky_loggerfn_t)(Sky_log_level_t level, const char *s);
-
-/*! \brief pointer to random bytes callback function
- */
-typedef int (*Sky_randfn_t)(uint8_t *rand_buf, uint32_t bufsize);
-
 Sky_status_t sky_open(Sky_errno_t *sky_errno, uint8_t *device_id,
     uint32_t id_len, uint32_t partner_id, uint32_t aes_key_id,
     uint8_t aes_key[16], void *state_buf, Sky_log_level_t min_level,
-    Sky_loggerfn_t logf, Sky_randfn_t rand_bytes);
+    Sky_loggerfn_t logf, Sky_randfn_t rand_bytes, Sky_timefn_t gettime);
 
 int32_t sky_sizeof_state(void *sky_state);
 

libel/protocol/proto.c
@@ -255,7 +255,8 @@ bool Rq_callback(
 
 int32_t serialize_request(Sky_ctx_t *ctx, uint8_t *buf, uint32_t buf_len)
 {
-    size_t rq_size, aes_padding_length, crypto_info_size, hdr_size, total_length;
+    size_t rq_size, aes_padding_length, crypto_info_size, hdr_size,
+        total_length;
     int32_t bytes_written;
     struct AES_ctx aes_ctx;
 
@@ -284,7 +285,7 @@ int32_t serialize_request(Sky_ctx_t *ctx, uint8_t *buf, uint32_t buf_len)
     rq.aps = rq.gsm_cells = rq.nbiot_cells = rq.cdma_cells = rq.lte_cells =
         rq.umts_cells = ctx;
 
-    rq.timestamp = (int64_t)time(NULL);
+    rq.timestamp = (int64_t)ctx->gettime(NULL);
 
     memcpy(rq.device_id.bytes, get_ctx_device_id(ctx), get_ctx_id_length(ctx));
     rq.device_id.size = get_ctx_id_length(ctx);

libel/unit_test.c
@@ -32,6 +32,21 @@ Sky_cache_t nv_space;
  */
 #define SCAN_LIST_SIZE 100
 
+/*! \brief time function
+ *
+ *  @param t where to save the time
+ *
+ *  @returns the time in seconds since the epoc (linux time)
+ */
+
+static time_t mytime(time_t *t)
+{
+    if (t != NULL) {
+        return time(t);
+    } else
+        return time(NULL);
+}
+
 /*! \brief set mac address. 30% are virtual AP
  *
  *  @param mac pointer to mac address
@@ -189,7 +204,8 @@ int main(int ac, char **av)
     srand((unsigned)time(NULL));
 
     if (sky_open(&sky_errno, mac /* device_id */, MAC_SIZE, 1, 1, aes_key,
-            nv_cache(), SKY_LOG_LEVEL_ALL, &logger, NULL) == SKY_ERROR) {
+            nv_cache(), SKY_LOG_LEVEL_ALL, &logger, NULL,
+            &mytime) == SKY_ERROR) {
         printf("sky_open returned bad value, Can't continue\n");
         exit(-1);
     }

libel/workspace.h
@@ -50,6 +50,7 @@ typedef struct sky_ctx {
     int (*logf)(Sky_log_level_t level, const char *s);
     int (*rand_bytes)(uint8_t *rand_buf, uint32_t buf_len);
     Sky_log_level_t min_level;
+    Sky_timefn_t gettime;
     int16_t len; /* number of beacons in list (0 == none) */
     Beacon_t beacon[TOTAL_BEACONS + 1]; /* beacon data */
     int16_t ap_len; /* number of AP beacons in list (0 == none) */

sample_client/sample_client.c
@@ -154,6 +154,20 @@ int logger(Sky_log_level_t level, const char *s)
     return 0;
 }
 
+/*! \brief time function
+ *
+ *  @param t where to save the time
+ *
+ *  @returns the time in seconds since the epoc (linux time)
+ */
+static time_t mytime(time_t *t)
+{
+    if (t != NULL) {
+        return time(t);
+    } else
+        return time(NULL);
+}
+
 /*! \brief validate fundamental functionality of the Embedded Library
  *
  *  @param argc count
@@ -195,11 +209,11 @@ int main(int argc, char *argv[])
     /* Comment in order to disable cache loading */
     nv_space = nv_cache(nv_space, 1);
 
-    timestamp = time(NULL); /* time scans were prepared */
+    timestamp = mytime(NULL); /* time scans were prepared */
     /* Initialize the Skyhook resources */
     if (sky_open(&sky_errno, config.device_mac, MAC_SIZE, config.partner_id,
             config.partner_id, config.key, nv_space, SKY_LOG_LEVEL_ALL, &logger,
-            &rand_bytes) == SKY_ERROR) {
+            &rand_bytes, &mytime) == SKY_ERROR) {
         printf("sky_open returned bad value, Can't continue\n");
         exit(-1);
     }

commit 90849b63a58c42733367d76a36f1b237f05d4d1d

[ELG-199] fix min_level - broke in commit 655b1e6

libel/libel.c
@@ -200,6 +200,7 @@ Sky_ctx_t *sky_new_request(
         (uint8_t *)&ctx->header.crc32 - (uint8_t *)&ctx->header.magic);
 
     ctx->cache = &cache;
+    ctx->min_level = sky_min_level;
     ctx->logf = sky_logf;
     ctx->rand_bytes = sky_rand_bytes;
     ctx->gettime = sky_time;