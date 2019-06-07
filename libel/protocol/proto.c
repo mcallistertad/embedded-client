fdc8d8c1e521d124d3aac7c0b313d79572899d5c
gcleary <gcleary@skyhook.com>
2019-06-07 12:19:46 -0400

    ELG-208 Include cryptoinfo size in response size
    RsHeader_size + CryptoInfo_size + Rs_size + 1

--- libel/protocol/proto.c
+++ libel/protocol/proto.c
@@ -244,7 +244,7 @@ bool Rq_callback(pb_istream_t *istream, pb_ostream_t *ostream, const pb_field_t 
 
 int32_t get_maximum_response_size(void)
 {
-    return RsHeader_size + Rs_size + 1;
+    return RsHeader_size + CryptoInfo_size + Rs_size + 1;
 }
 
 int32_t serialize_request(Sky_ctx_t *ctx, uint8_t *buf, uint32_t buf_len)
