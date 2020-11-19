b9e6f96bd5f23b90a1cbc3d26571ba45634bf193
Michael Gerber <mgerber@skyhook.com>
2020-11-19 13:43:17 -0500

    Re-arrange runtests.c to make test running easier

--- libel/runtests.c
+++ libel/runtests.c
@@ -5,6 +5,25 @@
 
 #include "unittest.h"
 
+Test_opts opts;
+
+/**************************
+ *     ADD TESTS HERE     *
+ **************************/
+static Test_rs runtests() {
+    Test_rs rs = { 0, 0 };
+
+    /* START TEST LIST */
+    /*RUN_TEST(beacon_test);*/
+    /*RUN_TEST(ap_plugin_vap_used);*/
+    /*RUN_TEST(test_utilities);*/
+    /*RUN_TEST(plugin_test);*/
+    RUN_TEST(new_tests);
+    /* END TEST LIST */
+
+    return rs;
+}
+
 static const char *optargs = "vh";
 
 static struct option longopts[] = {
@@ -23,7 +42,6 @@ static int usage(char **argv) {
 }
 
 int main(int argc, char **argv) {
-    Test_opts opts;
     for (;;) {
         int c = getopt_long(argc, argv, optargs, longopts, NULL);
         if (c == -1) {
@@ -42,11 +60,7 @@ int main(int argc, char **argv) {
         }
     }
 
-    Test_rs rs = { 0, 0 };
-    RUN_TEST(beacon_test);
-    RUN_TEST(ap_plugin_vap_used);
-    RUN_TEST(test_utilities);
-    RUN_TEST(plugin_test);
+    Test_rs rs = runtests();
 
     if (opts.verbose || rs.failed)
         fprintf(stdout, "%d tests run, %d failed\n", rs.ran, rs.failed);
