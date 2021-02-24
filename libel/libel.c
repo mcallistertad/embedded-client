3a47d7ca67bdb156bdefad64d1bdec9f7d2be554
gcleary <gcleary@skyhook.com>
2021-02-24 10:19:46 -0500

    Bump SW_VERSION to 13 (TBR)

--- libel/libel.c
+++ libel/libel.c
@@ -38,7 +38,7 @@
  * than just including the Git version string (since it will need to be updated
  * manually for every release) but cheaper bandwidth-wise.
  */
-#define SW_VERSION 8
+#define SW_VERSION 13
 
 /* Interval in seconds between requests for config params */
 #define CONFIG_REQUEST_INTERVAL (24 * SECONDS_IN_HOUR) /* 24 hours */
