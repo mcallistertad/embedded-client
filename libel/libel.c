5da258855972f961a2e5680e8cca9378b106460c
gcleary <gcleary@skyhook.com>
2021-08-27 10:46:01 -0400

    Increment SW VERSION

--- libel/libel.c
+++ libel/libel.c
@@ -35,7 +35,7 @@
  * than just including the Git version string (since it will need to be updated
  * manually for every release) but cheaper bandwidth-wise.
  */
-#define SW_VERSION 19
+#define SW_VERSION 20
 
 /* Interval in seconds between requests for config params */
 #define CONFIG_REQUEST_INTERVAL (24 * SECONDS_IN_HOUR) /* 24 hours */
